2b957d3156bfc8f72b9a64e205bcab3bb257d61d
刘宗源 <940101379@qq.com>
2019-01-23 09:06:11 +0800

    chore: add my demo

---
+++ examples/mydemo/app.js
@@ -0,0 +1,19 @@
+/* global Vue */
+
+/**
+ * Actual demo
+ */
+
+new Vue({
+
+  el: '#demo',
+
+  data: {
+    c:'myclass',
+    isShow: true,
+    strs: ['lzy', 'kkekek', 'sdkfkf']
+  },
+
+  created: function () {
+  }
+})
